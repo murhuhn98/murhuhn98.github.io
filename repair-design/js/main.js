de085f10e98e6033a73c44862815c5e755eed623
murhuhn <murhuhn@users.noreply.github.com>
2019-12-16 19:52:26 +0300

    feat: add project block

--- repair-design/js/main.js
+++ repair-design/js/main.js
@@ -27,4 +27,23 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+      loop: true,
+      pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+        },
+      navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+      },
+    });
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 35 + bullets.width() + 35 )
+    bullets.css('left', prev.width() + 35 )
+
 });
