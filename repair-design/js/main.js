232a4b7c77353067a9c021e28d832b8139f4b539
murhuhn <murhuhn@users.noreply.github.com>
2019-12-19 14:03:21 +0300

    feat: add animation in hero block and section__heading

--- repair-design/js/main.js
+++ repair-design/js/main.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 $(document).ready(function () {
   var modal = $('.modal'),
-      modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
@@ -28,36 +28,38 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 
-  
+
   $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
       $('#button-up').fadeIn('slow');
-  } else {
+    } else {
       $('#button-up').fadeOut();
-  }
-});
+    }
+  });
 
   $('#button-up').click(function () {
     $('body,html').animate({
       scrollTop: 0
-  }, 500);
-});
+    }, 500);
+  });
 
-  var mySwiper = new Swiper ('.swiper-container', {
-      loop: true,
-      pagination: {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
-        },
-      navigation: {
-       nextEl: '.swiper-button-next',
-       prevEl: '.swiper-button-prev',
-      },
-    });
-    var next = $('.swiper-button-next');
-    var prev = $('.swiper-button-prev');
-    var bullets = $('.swiper-pagination');
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
 
-    next.css('left', prev.width() + 35 + bullets.width() + 35 )
-    bullets.css('left', prev.width() + 35 );
+  next.css('left', prev.width() + 35 + bullets.width() + 35)
+  bullets.css('left', prev.width() + 35);
+
+  new WOW().init();
 });
